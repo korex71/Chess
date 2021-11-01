ee9d97ae5a2d8d8a6cafc57b47dfa406d09fad02
Gabriel <gabriel.silva@ecine.com.br>
2021-11-01 00:29:10 -0300

    Delegando inserção de peças e utilizando estruturação alfabetizada.

---
+++ Chess/Chess/ChessParty.cs
@@ -0,0 +1,55 @@
+﻿using Board;
+using Chess.Board;
+
+namespace Chess
+{
+    class ChessParty {
+        public BoardCF board { get; private set; }
+        private int turn;
+        private Color currentPlayer;
+
+        public ChessParty()
+        {
+            board = new BoardCF(8, 8);
+            turn = 1;
+            currentPlayer = Color.White;
+            insertPieces();
+        }
+
+        public void MakeMovement(Position origin, Position destiny)
+        {
+            Piece p = board.removePiece(origin);
+
+            p.incMoves();
+
+            Piece capturedPiece = board.removePiece(destiny);
+
+            board.insertPiece(p, destiny);
+        }
+
+        private void insertPieces()
+        {
+            board.insertPiece(new Tower(board, Color.White), new PositionChess('c', 1).toPosition());
+
+            board.insertPiece(new Tower(board, Color.Black), new PositionChess('a', 2).toPosition());
+            board.insertPiece(new Tower(board, Color.Black), new PositionChess('b', 2).toPosition());
+            board.insertPiece(new Tower(board, Color.Black), new PositionChess('c', 2).toPosition());
+            board.insertPiece(new Tower(board, Color.Black), new PositionChess('d', 2).toPosition());
+            board.insertPiece(new Tower(board, Color.Black), new PositionChess('e', 2).toPosition());
+            board.insertPiece(new Tower(board, Color.Black), new PositionChess('f', 2).toPosition());
+            board.insertPiece(new Tower(board, Color.Black), new PositionChess('g', 2).toPosition());
+            board.insertPiece(new Tower(board, Color.Black), new PositionChess('h', 2).toPosition());
+
+            board.insertPiece(new Tower(board, Color.White), new PositionChess('a', 7).toPosition());
+            board.insertPiece(new Tower(board, Color.White), new PositionChess('b', 7).toPosition());
+            board.insertPiece(new Tower(board, Color.White), new PositionChess('c', 7).toPosition());
+            board.insertPiece(new Tower(board, Color.White), new PositionChess('d', 7).toPosition());
+            board.insertPiece(new Tower(board, Color.White), new PositionChess('e', 7).toPosition());
+            board.insertPiece(new Tower(board, Color.White), new PositionChess('f', 7).toPosition());
+            board.insertPiece(new Tower(board, Color.White), new PositionChess('g', 7).toPosition());
+            board.insertPiece(new Tower(board, Color.White), new PositionChess('h', 7).toPosition());
+
+
+        }
+    }
+}
