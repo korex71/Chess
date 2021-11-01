f6faebce4d07b189f23c384387b8f460bd6299e7
Gabriel <gabriel.silva@ecine.com.br>
2021-10-31 22:18:30 -0300

    Adicionar arquivos de projeto.

---
+++ Chess/Board/Position.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess.Board
+{
+    public class Position
+    {
+        public int line { get; set; }
+        public int column { get; set; }
+
+        public Position(int line, int column)
+        {
+            this.line = line;
+            this.column = column;
+        }
+
+        public override string ToString()
+        {
+            return line
+                    + ", "
+                    + column;
+        }
+
+    }
+}
---
+++ Chess/Program.cs
@@ -0,0 +1,19 @@
+﻿using Chess.Board;
+using System;
+
+namespace Chess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Position P;
+
+            P = new Position(3, 4);
+
+            Console.WriteLine("Posição: " + P);
+
+            Console.ReadLine();
+        }
+    }
+}
